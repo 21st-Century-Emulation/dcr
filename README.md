59c908ec960d02746588e941356bb1b105790c47
David Tyler <david.tyler@metaswitch.com>
2021-04-20 19:56:42 +0100

    Initial commit of DCR microservice

---
+++ README.md
@@ -0,0 +1,3 @@
+# DCR
+
+Implementation of the DCR 8080 opcode as a crystal (w/ kemal) microservice
---
+++ test.sh
@@ -0,0 +1,24 @@
+docker build -q -t dcr .
+docker run --rm --name dcr -d -p 8080:8080 -e READ_MEMORY_API=http://localhost:8080/api/v1/debug/readMemory -e WRITE_MEMORY_API=http://localhost:8080/api/v1/debug/writeMemory dcr
+
+sleep 5
+
+RESULT=`curl -s --header "Content-Type: application/json" \
+  --request POST \
+  --data '{"opcode":37,"state":{"a":10,"b":1,"c":66,"d":5,"e":5,"h":0,"l":2,"flags":{"sign":false,"zero":false,"auxCarry":false,"parity":false,"carry":false},"programCounter":1,"stackPointer":2,"cycles":0}}' \
+  http://localhost:8080/api/v1/execute`
+EXPECTED='{"opcode":37,"state":{"a":10,"b":1,"c":66,"d":5,"e":5,"h":255,"l":2,"flags":{"sign":true,"zero":false,"auxCarry":false,"parity":true,"carry":false},"programCounter":1,"stackPointer":2,"cycles":5}}'
+
+docker kill dcr
+
+DIFF=`diff <(jq -S . <<< "$RESULT") <(jq -S . <<< "$EXPECTED")`
+
+if [ $? -eq 0 ]; then
+    echo -e "\e[32mDCR Test Pass \e[0m"
+    exit 0
+else
+    echo -e "\e[31mDCR Test Fail  \e[0m"
+    echo "$RESULT"
+    echo "$DIFF"
+    exit -1
+fi
